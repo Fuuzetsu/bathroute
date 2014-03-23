f8add2f24b811d9aaeda7cb1f2f8821456366188
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-23 22:22:32 +0000

    Int list testing

--- src/uk/co/fuuzetsu/bathroute/tests/NodeDeserialiserTests.java
+++ src/uk/co/fuuzetsu/bathroute/tests/NodeDeserialiserTests.java
@@ -4,6 +4,8 @@ import        fj.data.Option;
 import        java.lang.reflect.Field;
 import        java.lang.reflect.InvocationTargetException;
 import        java.lang.reflect.Method;
+import        java.util.ArrayList;
+import        java.util.List;
 import        junit.framework.TestCase;
 import        org.apache.commons.lang3.exception.ExceptionUtils;
 import        org.junit.*;
@@ -29,24 +31,58 @@ public class NodeDeserialiserTests extends TestCase {
         }
     }
 
-    public void testDeserialiser() {
+    private <A> void optParserTest(String mname, String arg, Option<A> expected) {
         try {
             NodeDeserialiser d = new NodeDeserialiser();
 
             Method method =
-                NodeDeserialiser.class.getDeclaredMethod("parseId",
+                NodeDeserialiser.class.getDeclaredMethod(mname,
                                                          String.class);
             method.setAccessible(true);
 
             @SuppressWarnings("unchecked")
-            Option<Integer> result = (Option<Integer>) method.invoke(d, "12");
-            assertTrue(optEq(result, Option.some(12)));
+            Option<A> result = (Option<A>) method.invoke(d, arg);
+            assertTrue(optEq(result, expected));
         } catch (IllegalArgumentException
                  |IllegalAccessException
                  |InvocationTargetException
                  |NoSuchMethodException e) {
-            System.out.println("Reflection exception in testDeserialiser\n"
+            System.out.println("Reflection exception testing " + mname + "\n"
                                + ExceptionUtils.getStackTrace(e));
         }
     }
+
+    public void testIdParser() {
+        Option<Integer> n = Option.none();
+        Option<Integer> r = Option.some(12);
+        optParserTest("parseId", "12", r);
+        optParserTest("parseId", "hello", n);
+    }
+
+    public void testIntList() {
+        List<Integer> r = new ArrayList();
+        r.add(1); r.add(2); r.add(3); r.add(4);
+        String input = "1 2 3 4";
+
+        try {
+            NodeDeserialiser d = new NodeDeserialiser();
+
+            Method method =
+                NodeDeserialiser.class.getDeclaredMethod("parseIntList",
+                                                         String.class);
+            method.setAccessible(true);
+
+            @SuppressWarnings("unchecked")
+            List<Integer> result = (List<Integer>) method.invoke(d, input);
+            assertEquals(r, result);
+        } catch (IllegalArgumentException
+                 |IllegalAccessException
+                 |InvocationTargetException
+                 |NoSuchMethodException e) {
+            System.out.println("Reflection exception testing parseIntList\n"
+                               + ExceptionUtils.getStackTrace(e));
+        }
+
+    }
+
 }
