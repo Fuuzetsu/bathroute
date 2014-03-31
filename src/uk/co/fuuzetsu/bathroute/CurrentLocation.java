ec71a986fbae0b6c5ad6a68e0736a3d4e4899a12
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-03-31 02:01:33 +0100

    Whitespace only

--- src/uk/co/fuuzetsu/bathroute/CurrentLocation.java
+++ src/uk/co/fuuzetsu/bathroute/CurrentLocation.java
@@ -9,41 +9,41 @@ import android.location.LocationManager;
 import android.os.Bundle;
 
 public class CurrentLocation extends Activity {
-	private MapView mapView;
+        private MapView mapView;
 
-	private MyLocationNewOverlay myLocationoverlay;
-	private LocationManager myLocationmanger;
+        private MyLocationNewOverlay myLocationoverlay;
+        private LocationManager myLocationmanger;
 
-	/** Called when the activity is first created. */
+        /** Called when the activity is first created. */
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initializemap();
-		myLocationoverlay = new MyLocationNewOverlay(this, mapView);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                initializemap();
+                myLocationoverlay = new MyLocationNewOverlay(this, mapView);
 
-		myLocationoverlay.enableFollowLocation();
-		myLocationoverlay.enableMyLocation();
+                myLocationoverlay.enableFollowLocation();
+                myLocationoverlay.enableMyLocation();
 
-		myLocationoverlay.setDrawAccuracyEnabled(false);
-		myLocationoverlay.runOnFirstFix(new Runnable() {
-			@Override
-			public void run() {
-				mapView.getController().animateTo(
-						myLocationoverlay.getMyLocation());
-			}
-		});
-		mapView.getOverlays().add(myLocationoverlay);
-	}
+                myLocationoverlay.setDrawAccuracyEnabled(false);
+                myLocationoverlay.runOnFirstFix(new Runnable() {
+                        @Override
+                        public void run() {
+                                mapView.getController().animateTo(
+                                                myLocationoverlay.getMyLocation());
+                        }
+                });
+                mapView.getOverlays().add(myLocationoverlay);
+        }
 
-	public void initializemap() {
-		mapView = (MapView) this.findViewById(R.id.mapview);
-		mapView.setTileSource(TileSourceFactory.MAPNIK);
-		mapView.setBuiltInZoomControls(true);
-		mapView.setMultiTouchControls(true);
-		mapView.setMaxZoomLevel(19);
+        public void initializemap() {
+                mapView = (MapView) this.findViewById(R.id.mapview);
+                mapView.setTileSource(TileSourceFactory.MAPNIK);
+                mapView.setBuiltInZoomControls(true);
+                mapView.setMultiTouchControls(true);
+                mapView.setMaxZoomLevel(19);
 
-		mapView.getController().setZoom(17);
-	}
+                mapView.getController().setZoom(17);
+        }
 }
