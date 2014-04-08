2cc6a96527c5801cd9958cd337d1e2c239924c57
rs682 <rs682@bath.ac.uk>
2014-04-08 18:03:00 +0100

    Update EventsActivity.java

--- src/uk/co/fuuzetsu/bathroute/EventsActivity.java
+++ src/uk/co/fuuzetsu/bathroute/EventsActivity.java
@@ -1,42 +1,50 @@
 package uk.co.fuuzetsu.bathroute;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import java.util.LinkedList;
-import java.util.List;
-import uk.co.fuuzetsu.bathroute.Engine.DataStore;
-import uk.co.fuuzetsu.bathroute.Engine.Event;
+import android.widget.TextView;
+
+import android.support.v4.app.Fragment;
 
 public class EventsActivity extends Fragment {
 
-    @Override
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.events, container, false);
+        final View rootView = inflater.inflate(R.layout.events, container,
+                false);
 
-        DataStore ds = DataStore.getInstance();
-        List<String> evs = new LinkedList<String>();
+        final ListView lv = (ListView) rootView
+                .findViewById(R.id.events_list_view);
+        
+        String[] values = new String[] { "Example Event"};
+        
+        ArrayAdapter<String> lvadapter = new ArrayAdapter<String>(
+                rootView.getContext(), android.R.layout.simple_list_item_1,
+                values);
+        
+        lv.setOnItemClickListener(new OnItemClickListener() {
 
-        for (Event e : ds.getEvents()) {
-            evs.add(e.toString());
-        }
-
-        final ListView lv = (ListView) rootView.findViewById(R.id.events_list_view);
-
-        // String[] values = new String[] { "Create New Event", "View Existing Events" };
-        String[] values = evs.toArray(new String[0]);
-
-        ArrayAdapter<String> lvadapter
-            = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_1, values);
+            public void onItemClick(AdapterView<?> adapter, View v, int position, long id) {
+            	Intent i = new Intent(rootView.getContext(), EventDetailsActivity.class);
+            	startActivity(i);
+            }
+        });
 
         lv.setAdapter(lvadapter);
-
-        return rootView;
-    }
+        
+		return rootView;
+	}
 }
+
+        
