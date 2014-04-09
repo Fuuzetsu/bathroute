607f2dfaa0218787c524530e8ead95fdc3e1680c
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-04-09 19:49:31 +0100

    Fill in event list with the actual events we have.

--- src/uk/co/fuuzetsu/bathroute/EventDetailsActivity.java
+++ src/uk/co/fuuzetsu/bathroute/EventDetailsActivity.java
@@ -2,29 +2,26 @@ package uk.co.fuuzetsu.bathroute;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import android.view.View;
-
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-
-import android.view.ViewGroup;
-
 public class EventDetailsActivity extends Activity {
 
-	//store the title
-	private TextView title;
-	//store event description
-	private TextView time;
-	private TextView date;
-	private TextView description;
-	
-	
-	  @Override
-	  protected void onCreate(Bundle savedInstanceState) {
+    //store the title
+    private TextView title;
+    //store event description
+    private TextView time;
+    private TextView date;
+    private TextView description;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eventdetails);
@@ -33,6 +30,6 @@ public class EventDetailsActivity extends Activity {
         date = (TextView)findViewById(R.id.eventDate);
         time = (TextView)findViewById(R.id.eventTime);
         description = (TextView)findViewById(R.id.eventDescription);
-        
+
     }
 }
--- src/uk/co/fuuzetsu/bathroute/EventsActivity.java
+++ src/uk/co/fuuzetsu/bathroute/EventsActivity.java
@@ -3,48 +3,57 @@ package uk.co.fuuzetsu.bathroute;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import android.support.v4.app.Fragment;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import uk.co.fuuzetsu.bathroute.Engine.DataStore;
+import uk.co.fuuzetsu.bathroute.Engine.Event;
 
 public class EventsActivity extends Fragment {
 
-	@Override
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.events, container,
-                false);
+                                               false);
 
-        final ListView lv = (ListView) rootView
-                .findViewById(R.id.events_list_view);
-        
-        String[] values = new String[] { "Example Event"};
-        
-        ArrayAdapter<String> lvadapter = new ArrayAdapter<String>(
-                rootView.getContext(), android.R.layout.simple_list_item_1,
-                values);
-        
+        final ListView lv = (ListView) rootView.findViewById(R.id.events_list_view);
+
+        DataStore ds = DataStore.getInstance();
+        List<Event> evs = ds.getEvents();
+        Map<Integer, Event> evm = new HashMap<Integer, Event>();
+        String[] values = new String[evs.size()];
+
+        for (Integer i = 0; i < evs.size(); i++) {
+            evm.put(i, evs.get(i));
+            values[i] = evs.get(i).getDescription();
+        }
+
+        ArrayAdapter<String> lvadapter =
+            new ArrayAdapter<String>(rootView.getContext(),
+                                     android.R.layout.simple_list_item_1, values);
+
         lv.setOnItemClickListener(new OnItemClickListener() {
 
-            public void onItemClick(AdapterView<?> adapter, View v, int position, long id) {
-            	Intent i = new Intent(rootView.getContext(), EventDetailsActivity.class);
-            	startActivity(i);
-            }
-        });
+                public void onItemClick(AdapterView<?> adapter, View v, int position, long id) {
+                    Intent i = new Intent(rootView.getContext(), EventDetailsActivity.class);
+                    startActivity(i);
+                }
+            });
 
         lv.setAdapter(lvadapter);
-        
-		return rootView;
-	}
-}
 
-        
+        return rootView;
+    }
+}
